9ddfe37e7c48a61f19dc22ec1b145c2ca1cae78f
VinayMandhaan <vinaymandhaan29@gmail.com>
2025-03-20 20:07:25 +0400

    API Key Added

--- app/api/job-descriptions/route.ts
+++ app/api/job-descriptions/route.ts
@@ -2,9 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import OpenAI from 'openai'
 
 const openai = new OpenAI({
-    dangerouslyAllowBrowser: true,
-    apiKey: "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",
-  })
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
 export async function POST(request: NextRequest) {
     try {
